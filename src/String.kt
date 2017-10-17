575cd80c6927b00d4db3548929415a91155552fd
rifalfahrudin <rifalfahrudin@gmail.com>
2017-10-17 11:47:17 +0700

    WhileLoop

--- src/String.kt
+++ src/String.kt
@@ -1,3 +1,16 @@
 /**
  * Created by SAINTEK 101 on 10/17/2017.
  */
+fun main(args: Array<String>) {
+    var title= "selamat datang"
+    println(title)
+    println("title:"+title)
+    println("title: $title")
+    var name="rifal"+"fahrudin"
+    println("name:$name")
+    println("second char :"+title[1])
+    println("title:"+title.toLowerCase())
+    println("title:"+title.toUpperCase())
+    println("title:"+title.split(""))
+    println("title:"+title.trim())
+}
--- src/WhileLoop.kt
+++ src/WhileLoop.kt
@@ -1,3 +1,11 @@
 /**
  * Created by SAINTEK 101 on 10/17/2017.
  */
+fun main(args: Array<String>) {
+    var i=1
+    while (i<100){
+        println(" RIFAL")
+        i++
+
+    }
+}
