32daa4033e4eb62143e43f1b4fbf1d999d13693c
rifalfahrudin <rifalfahrudin@gmail.com>
2017-10-23 20:21:56 +0700

    lanjutan

---
+++ src/P2_26_GlobalAndLocalVariable.kt
@@ -0,0 +1,11 @@
+var fullname = "Guntur Desi Hidayani" //global var
+fun display():Unit{
+    println(fullname)
+    //print("name:" + name)
+}
+
+fun main(args: Array<String>) {
+    var name = "Guntur" //local var
+    println(fullname)
+    print("name:" + name)
+}
